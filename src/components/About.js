1001daff9877a1eed9e24e97c1e8a0efd9baa475
ravine13 <kuriaderek@gmail.com>
2023-10-27 09:24:38 +0300

    completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ function About(props) {
     <aside>
       <img src={image || "https://via.placeholder.com/215"} alt="blog logo" />
       <p>{aboutText}</p>
+      <p>{"About this blog"}</p>
     </aside>
   );
 }
